f1ed19e0507c7947281d46e09b192125c4e76ccf
Jaglyser <joelwblomster@gmail.com>
2021-10-05 22:19:22 +0200

    v2

--- app/Http/Controllers/RawController.php
+++ app/Http/Controllers/RawController.php
@@ -13,14 +13,12 @@ class RawController extends Controller
 
         // mt_rand uses Mersenne Twister pseudorandom function, which is actually pretty good.
         // generate a random key to the array of symbols.
-
-        $symbols = SYMBOLS;
-        
+        $symbols = ['9', '10', 'J', 'Q', 'K', 'A', 'cat', 'dog', 'monkey', 'bird']; 
         $board = [];
 
 
         for($n = 0; $n < 15; $n++){
-            $key = mt_rand(0, 9);
+            $key = mt_rand(0, 2);
             $board[] = $symbols[$key];
         }
         return $board;
@@ -30,16 +28,10 @@ class RawController extends Controller
     // generates the matching numbers to the board
     public function generateBoardNum(){
         for($i = 0; $i < 3; $i++){
-
             for($j = 0; $j < 5; $j++){
-
                 $entries[] = $i + ($j * 3);
-
             }
-            
-
         }
-
         return $entries;
     }
 
@@ -47,9 +39,16 @@ class RawController extends Controller
         $board = $this->generateBoard();
 
         // constants
-        $bet = BET;
-        $paylines = PAYLINES;
+        $bet = 100;
+        $paylines = [
+            "0 3 6 9 12",
+            "1 4 7 10 13",
+            "2 5 8 11 14",
+            "0 4 8 10 12",
+            "2 4 6 10 14"
+        ];
 
+        
 
         // variables which are used later to determine whether a payline is winning or not
         $previous = 0;
@@ -61,18 +60,12 @@ class RawController extends Controller
         $multiplier = 0;
         $win = 0;
 
-
         // generate array with all matching numbers to the board
         $entries = $this->generateBoardNum();
-
-
         $multi_board = array_combine($entries, $board);
-
-
         $fpaylines = [];
 
         foreach($paylines as $payline){
-
             // when two or more symbols are the same next to each other the system gathers them in $connected
             // it also counts how many fields the array connected contains
             // so when looping through the paylines, it resets $connected and $streak when looking through a new payline
@@ -84,7 +77,6 @@ class RawController extends Controller
 
             // loops through all the matching numbers in the recently split payline
             foreach($characters as $char){
-
                 // using the number as every symbol has been indexed to the number
                 // takes the current symbol
                 $current = $multi_board[$char];
@@ -92,46 +84,32 @@ class RawController extends Controller
                 // checks if the current symbol is the same as the previous, and if so starts a streak
                 // if not it resets the streak
                 if($current == $previous){
-
                     $connected[] = $current;
-
                     $streak = count($connected) + 1;
 
                 } else {
-
                     $connected = [];
-
                     $streak = 0;
-
                 }
 
                 // if the streak is 3 symbols in a row it makes the win multiplier 20%
                 if($streak == 3){
-
                     $multiplier = 0.2;
-
                     // $fpaylines[] =  array($payline=>3);
-                    
                 }
 
                 // 4 symbol streak = 200%
                 if($streak == 4){
-
                     $multiplier = 2;
-
                     // $fpaylines[] =  array($payline=>4);
-
                 }
 
                 // 5 symbol steak = 1000%
                 if($streak == 5){
-
                     $multiplier = 10;
-
                     // $fpaylines[] =  array($payline=>5);
 
                 }
-
                 // remembers the previous symbol
                 $previous = $multi_board[$char];
             }
@@ -139,18 +117,10 @@ class RawController extends Controller
             // the total win sum
             $win += $multiplier*100;
 
-
-            // adds the winning payline, and number of symbols in a row
-            if($multiplier==0.2){
-                $fpaylines[] = array($payline=>3);
+            if($multiplier != 0){
+                $fpaylines[] = $this->payLineNunber($payline, $multiplier);
             }
-            if($multiplier==2){
-                $fpaylines[] = array($payline=>4);
-            }
-            if($multiplier==10){
-                $fpaylines[] = array($payline=>5);
-            }
-
+            
             // resets the multiplier
             $multiplier = 0;
         }
@@ -159,8 +129,7 @@ class RawController extends Controller
         $array_json = ['board' => $board, 'paylines' => $fpaylines, 'bet_amount' => $bet, 'total_win' => $win];
         
         // prints to command line and encodes to JSON in forced object form
-        echo json_encode($array_json, JSON_FORCE_OBJECT);
-
+        echo json_encode($array_json, JSON_PRETTY_PRINT);
 
         // works for the API, but wont print to commandline.
         return response()->json($array_json);
@@ -170,19 +139,14 @@ class RawController extends Controller
         $this->findWinner();
     }
 
+    public function payLineNunber($payline, $multiplier){
+        $pay_line_number = [
+            0.2 => [$payline => 3],
+            2 => [$payline => 4],
+            10 => [$payline=>5],
+        ];
+
+        return $pay_line_number[$multiplier];
+    }
+
 }
-
-
-// defines constants
-
-define('BET', 100);
-
-define('PAYLINES', [
-    "0 3 6 9 12",
-    "1 4 7 10 13",
-    "2 5 8 11 14",
-    "0 4 8 10 12",
-    "2 4 6 10 14"
-]);
-
-define('SYMBOLS', ['9', '10', 'J', 'Q', 'K', 'A', 'cat', 'dog', 'monkey', 'bird']);
